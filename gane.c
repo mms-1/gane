4e11c3d2290c3a9b88a9051dcbc5f2e2c24a0787
Maksim Mikhalchuk <maksim.mikhalchuk@gmail.com>
2013-10-20 20:16:17 +0200

    Some code added. Not working - needed Linux

--- gane.c
+++ gane.c
@@ -1,1 +1,48 @@
-#
+/* gane - collects some symbols or symbol sets from the input stream.
+	Some comment
+*/
+/* Maksim Mikhalchuk <maksim.mikhalchuk@gmail.com> */
+
+// #include <config.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "system.h"
+
+#include "error.h"
+#include "long-options.h"
+
+/* The official name of this program (e.g., no 'g' prefix).  */
+#define PROGRAM_NAME "gane"
+
+#define AUTHORS proper_name ("Maksim Mikhalchuk")
+
+void usage (int status)
+{
+  if (status != EXIT_SUCCESS)
+    emit_try_help ();
+  else
+    {
+      printf (_("\
+				Usage: %s [STRING]...\n\
+				or:  %s OPTION\n\
+				"),
+              program_name, program_name);
+
+      fputs (_("\
+				Repeatedly output a line with all specified STRING(s), \
+				or 'y'.\n\
+				\n\
+			"), stdout);
+      fputs (HELP_OPTION_DESCRIPTION, stdout);
+      fputs (VERSION_OPTION_DESCRIPTION, stdout);
+      emit_ancillary_info ();
+    }
+  exit (status);
+}
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello\n");
+	return 0;
+}
